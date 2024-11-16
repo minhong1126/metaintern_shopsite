abca645f0ae1d28e507a54d00455d8245486f645
minhong1126 <mjhong159357@gmail.com>
2024-11-16 23:00:28 +0900

    결제 완료 시 장바구니 비우기 기능 구현

--- project/src/pages/PaySuccessPage.tsx
+++ project/src/pages/PaySuccessPage.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { cartState, productState } from "../state/state";
 import { useState, useEffect } from "react";
 
 function PaySuccessPage() {
   const nav = useNavigate();
-  const cart = useRecoilValue(cartState);
+  const [cart, setCart] = useRecoilState(cartState);
   const productList = useRecoilValue(productState);
   const [total, setTotal] = useState(0);
 
@@ -28,8 +28,15 @@ function PaySuccessPage() {
     getTotal();
   }, []);
 
+  function emptyCart(){
+    setCart({
+      index: [],
+      cnt: [],
+    })
+  }
 
   function gotoCart(){
+    emptyCart();
     nav('/cart');
   }
 
@@ -37,18 +44,18 @@ function PaySuccessPage() {
     <>
     <div className='h-[1000px] w-[430px]'>
       <header className="flex items-center justify-center bg-black text-white h-[69px] w-full p-[31px]" />
-      <div className="flex flex-col align-center items-center">
-        <div>
-          <p> 결제 완료! </p>
-          <p> 총 {cart.index.length}개의 상품을 구매하셨습니다. </p>
+      <div className="flex flex-col align-center items-center mt-[291px]">
+        <div className="flex flex-col items-center">
+          <p className="font-extrabold text-[30px]"> 결제 완료! </p>
+          <p className="mt-[7px]"> 총 {cart.index.length}개의 상품을 구매하셨습니다. </p>
         </div>
-        <div>
-          <p> 총 결제 금액 </p>
-          <p> {total} </p>
+        <div className="flex flex-col items-center mt-[18px]">
+          <p className="font-bold"> 총 결제 금액 </p>
+          <p className="font-bold text-[24px] mt-[9px]"> {total}원 </p>
         </div>
-        <div>
+        <div className="mt-[33px]">
           <button 
-            className="flex w-[213px] h-[30px] mt-[13px] items-center justify-center bg-[#FFEF64] text-[10px] rounded-full"
+            className="flex w-[385px] h-[58px] mt-[13px] items-center justify-center bg-[#FFEF64] text-[20px] font-normal rounded-full"
             onClick={gotoCart}>
             상품 목록 보기
           </button>
