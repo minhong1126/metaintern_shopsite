73076425b8f6af433305673fb3c8c5a6e0b08b3c
minhong1126 <mjhong159357@gmail.com>
2024-11-19 22:59:21 +0900

    node_modules 재 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,9 +6,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
