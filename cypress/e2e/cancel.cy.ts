be1e1e9a376c896ecba1d9b709f69e18af96797c
minhong1126 <mjhong159357@gmail.com>
2024-11-29 14:36:38 +0900

    결제 중 취소 테스트 코드 추가

--- cypress/e2e/cancel.cy.ts
+++ cypress/e2e/cancel.cy.ts
@@ -1,16 +1,35 @@
 describe('결제 중 취소', () => {
-  it('뭐가 있지', () => {
-    
-  })
-
   /* ==== Test Created with Cypress Studio ==== */
   it('카드 등록 전 취소', function() {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/');
-    cy.get(':nth-child(1) > .pl-\\[17px\\] > .flex > .bg-black').click();
-    cy.get('[d="M18 6h-2c0-2.21-1.79-4-4-4S8 3.79 8 6H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-6-2c1.1 0 2 .9 2 2h-4c0-1.1.9-2 2-2zm6 16H6V8h2v2c0 .55.45 1 1 1s1-.45 1-1V8h4v2c0 .55.45 1 1 1s1-.45 1-1V8h2v12z"]').click();
+    cy.get('button.h-\\[24px\\] > .h-\\[24px\\]').click();
     cy.get('.w-\\[385px\\]').click();
-    cy.get('.h-\\[20px\\]').click();
+    cy.get('.h-\\[20px\\] > path').click();
     /* ==== End Cypress Studio ==== */
   });
+
+  it('카드 등록 중 취소', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('/');
+    cy.get('button.h-\\[24px\\] > .h-\\[24px\\]').click();
+    cy.get('.w-\\[385px\\]').click();
+    cy.get('.flex > svg').click();
+    cy.get(':nth-child(1) > .default-input-styles').clear('7777777777777777');
+    cy.get(':nth-child(1) > .default-input-styles').type('7777777777777777');
+    cy.get('[name="year"]').clear();
+    cy.get('[name="year"]').type('77');
+    cy.get('[name="month"]').clear();
+    cy.get('[name="month"]').type('7');
+    cy.get(':nth-child(3) > .default-input-styles').clear();
+    cy.get(':nth-child(3) > .default-input-styles').type('ee');
+    cy.get(':nth-child(4) > .default-input-styles').clear();
+    cy.get(':nth-child(4) > .default-input-styles').type('777');
+    cy.get('[name="pw1"]').clear();
+    cy.get('[name="pw1"]').type('7');
+    cy.get('[name="pw2"]').clear();
+    cy.get('[name="pw2"]').type('7');
+    cy.get('.h-\\[28px\\]').click();
+    /* ==== End Cypress Studio ==== */
+  })
 })
